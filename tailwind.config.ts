4a3fe37c3a477b67b7c535adcef7a3b667c88935
Nick Trombley <trombley.nick@gmail.com>
2024-09-18 15:00:38 -0400

    Tailwind setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,12 @@
 import type { Config } from 'tailwindcss';
 
 export default {
-	darkMode: ['class'],
+	darkMode: ['class', '[data-theme="dark"]'],
 	content: ['./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}'],
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: [
-					'Inter"',
-					'ui-sans-serif',
-					'system-ui',
-					'sans-serif',
-					'Apple Color Emoji"',
-					'Segoe UI Emoji"',
-					'Segoe UI Symbol"',
-					'Noto Color Emoji"'
-				]
+				sans: ['Inter', 'system-ui', 'sans-serif']
 			},
 			borderRadius: {
 				lg: 'var(--radius)',
